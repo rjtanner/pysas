2c8ef9f752968ed72217101d380556e6eb27b21e
Ryan Tanner <58440445+rjtanner@users.noreply.github.com>
2023-06-07 12:26:54 -0400

    Add Pipeline

--- __init__.py
+++ __init__.py
@@ -11,5 +11,6 @@ from . import configutils
 from .version import *
 from .taskinfo import taskinfo
 from .odfcontrol import odfcontrol
+from .pipeline import pipeline
 
 __version__ = f'pysas - (pysas-{VERSION}) [{SAS_RELEASE}-{SAS_AKA}]'
--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -46,70 +46,127 @@ class SASpipeline(object):
     SAS analysis pipeline object.
     
     """
-    def __init__(self,odfid,obs_dir=None):
-        self.odfid = odfid
-
-        # Where are we?
-        startdir = os.getcwd()
-
-        # Brief check to see if obs_dir was 
-        # given on SASpipeline creation.
-        if self.obs_dir != None:
-            obs_dir = self.obs_dir
-
-        # Start checking obs_dir
-        if obs_dir == None:
-            obs_dir = sas_cfg.get("sas", "data_dir")
-            if os.path.exists(obs_dir):
-                self.obs_dir = obs_dir
+    def __init__(self,pipefile=None,pipename=None):
+        self.pipeline = []
+        cwd = os.getcwd()
+        if pipefile != None:
+            if not os.path.isfile(pipefile):
+                print(f'Pipeline file {pipefile} does not exist!')
+                print(f'Creating a new basic pipeline file!')
+                self.pipefile = self.copy_basic(pipefile)
             else:
-                self.obs_dir = startdir
+                self.pipefile = self.read(pipefile)
         else:
-            self.obs_dir = obs_dir
+            if pipename:
+                # Add to look in pipeline library
+                self.create_basic(pipename)
+            else:
+                print('Creating a generic pipeline.')
+                self.pipeline = self.read(os.path.join(os.path.dirname(os.path.abspath(__file__)),'pipe_template.txt'))
+        
+        if pipename == None:
+            self.pipename = 'default'
 
-        # If data_dir was not given as an absolute path, it is interpreted
-        # as a subdirectory of startdir.
-        if self.data_dir[0] != '/':
-            self.data_dir = os.path.join(startdir, self.data_dir)
-        elif self.data_dir[:2] == './':
-            self.data_dir = os.path.join(startdir, self.data_dir[2:])
+    def set_pipename(self,pipename):
+        self.pipename = pipename
 
-        # Check if data_dir exists. If not then create it.
-        # Save comments for the logger created later.
-        logcomment = ''
-        if not os.path.isdir(self.data_dir):
-            logcomment = f'{self.data_dir} does not exist. Creating it!'
-            os.mkdir(self.data_dir)
-            logcomment = logcomment + '\n' + f'{self.data_dir} has been created!'
-
-        # Set directory for the observation.
-        self.obs_dir  = os.path.join(self.data_dir,self.odfid)
-
-    def get_obs_dir(self):
+    def get_obs_dir(self,odfid):
         pass
 
-    def add_command(self,cmd,inargs):
-        pass
+    def add_command(self,cmd,inargs=''):
+        sas_path = os.environ['SAS_PATH'].split(':')
+        for path in sas_path:
+            parfile = cmd + '.par'
+            files = glob.glob(os.path.join(path, 'config', parfile))
+            if files:
+                if files[0] != '': cmdnotfound = True
+                break
 
-    def remove_command(self,cmd):
-        pass
+        if cmdnotfound:
+            raise Exception(f'Does not exist any command named \'{cmd}\'. Wrong syntax?')
+        self.pipeline.append(f'{cmd} {inargs}'.strip())
 
-    def show(self):
-        pass
+    def remove_command(self,cmd=None,linenum=None):
+        if linenum:
+            if cmd and not (cmd in self.pipeline[linenum]):
+                print(f'Command {cmd} not found in line number {linenum}.')
+                return
+            lineremove = linenum
+        else:
+            linelist = []
+            for i,line in enumerate(self.pipeline):
+                line = line.split(' ',1)
+                if cmd == line[0]:
+                    linelist.append(i)
+            if len(linelist) == 0:
+                print(f'No lines with {cmd} were found!')
+                return
+            elif len(linelist) == 1:
+                lineremove = linelist[0]
+            elif len(linelist) > 1:
+                print(f'\nMultiple lines with the command \"{cmd}\" found.')
+                print(f'Specify which line to remove using remove_command(\'{cmd}\',linenum=???).\n')
+                for cmdline in linelist:
+                    print(f'{cmdline} '+self.pipeline[cmdline])
+                return
+
+        self.pipeline.pop(lineremove)
+
+    def show_pipeline(self,with_numbers=True):
+        for i,line in enumerate(self.pipeline):
+            if with_numbers:
+                print(f'{i} '+line)
+            else:
+                print(line)
 
     def change_inargs(self,cmd,inargs):
         pass
 
     def read(self,filename):
+        with open(filename) as f:
+            pipeline = f.readlines()
+        for line in pipeline:
+            self.pipeline.append(line.strip())
+        if 'XXpipenameXX' in self.pipeline[0]:
+            self.pipename = 'default'
+            self.pipeline[0] = f'# {self.pipename}'
+        else:
+            self.pipename = self.pipeline[2:]
+
+        return self.pipeline
+
+    def write(self,filename=''):
+        if not filename:
+            filename = f'{self.pipename}'
+        cwd = os.getcwd()
+        # If not an absolute path then assumed to be in the current working directory.
+        if filename[0] != '\\':
+            filename = os.path.join(cwd,filename)
+        if filename[-4:] != '.txt':
+            filename = filename + '.txt'
+        with open(filename,'w') as f:
+            f.writelines(line + os.linesep for line in self.pipeline)
+
+    def copy(self,infile,outfile):
+        self.pipeline = self.read(infile)
+        self.write(outfile)
+
+    def copy_basic(self,filename,pipefile=None):
+        tempfile = os.path.join(os.path.dirname(os.path.abspath(__file__)),'pipe_template.txt')
+        shutil.copy(tempfile,filename)
+        self.read(filename)
+        return filename
+    
+    def create_basic(self,pipename=None):
+        filename = os.path.join(os.path.dirname(os.path.abspath(__file__)),'pipe_template.txt')
+        self.read(filename)
+        self.pipename = pipename
+        return filename
+
+    def delete(self):
         pass
 
-    def write(self,filename):
-        pass
-
-    def copy(self,filename):
-        pass
-
-    def run(self):
+    def run(self,odfid):
         pass
 
 
