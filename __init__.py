1bc79b91d8293cc0da500fafaa7222fa856f406e
Ryan Tanner <58440445+rjtanner@users.noreply.github.com>
2023-06-09 10:22:22 -0400

    Revert "Add Pipeline"
    This reverts commit 2c8ef9f752968ed72217101d380556e6eb27b21e.

--- __init__.py
+++ __init__.py
@@ -11,6 +11,5 @@ from . import configutils
 from .version import *
 from .taskinfo import taskinfo
 from .odfcontrol import odfcontrol
-from .pipeline import pipeline
 
 __version__ = f'pysas - (pysas-{VERSION}) [{SAS_RELEASE}-{SAS_AKA}]'
